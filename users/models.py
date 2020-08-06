39a241211901ebea58d393f8849692623d92edd7
Tanvi Aditya <tanviaditya1@gmail.com>
2020-08-07 01:12:37 +0530

    Back to you after all the loss

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
-	def save(self):
+	def save(self ,*args, **kwargs):
 		#overiding self method
 		super().save()
 		img=Image.open(self.image.path)
